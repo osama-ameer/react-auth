9df9e76fd37587faa85802af3de082570ad0674e
osama-ameer <osamaameer15@gmail.com>
2022-06-27 15:56:10 +0500

    flexmonster configured

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ const App = () => {
       setCurrentUser(user);
       setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
       setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
+
+      // console.log(currentUser);
     }
 
     EventBus.on("logout", () => {
@@ -48,15 +50,15 @@ const App = () => {
   return (
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/"} className="navbar-brand">
+        <div className="navbar-brand">
           FlexMonster
-        </Link>
+        </div>
         <div className="navbar-nav mr-auto">
-          <li className="nav-item">
+          {/* <li className="nav-item">
             <Link to={"/home"} className="nav-link">
               Home
             </Link>
-          </li>
+          </li> */}
 
           {showModeratorBoard && (
             <li className="nav-item">
@@ -69,30 +71,30 @@ const App = () => {
           {showAdminBoard && (
             <li className="nav-item">
               <Link to={"/admin"} className="nav-link">
-                Admin Board
+                Admin Panel
               </Link>
             </li>
           )}
 
-          {currentUser && (
+          {/* {currentUser && (
             <li className="nav-item">
               <Link to={"/user"} className="nav-link">
                 User
               </Link>
             </li>
-          )}
+          )} */}
         </div>
 
         {currentUser ? (
           <div className="navbar-nav ml-auto">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to={"/profile"} className="nav-link">
                 {currentUser.username}
               </Link>
-            </li>
+            </li> */}
             <li className="nav-item">
               <a href="/login" className="nav-link" onClick={logOut}>
-                LogOut
+                Logout
               </a>
             </li>
           </div>
@@ -104,25 +106,25 @@ const App = () => {
               </Link>
             </li>
 
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link to={"/register"} className="nav-link">
                 Sign Up
               </Link>
-            </li>
+            </li> */}
           </div>
         )}
       </nav>
 
       <div className="container mt-3">
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Login/>} />
           <Route path="/home" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
           <Route path="/profile" element={<Profile/>} />
           <Route path="/user" element={<BoardUser/>} />
           <Route path="/mod" element={<BoardModerator/>} />
-          <Route path="/admin" element={<BoardAdmin/>} />
+          <Route path="/admin" user={currentUser} element={<BoardAdmin/>} />
          
         </Routes>
       </div>
--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -1,48 +1,162 @@
-import React, { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useImperativeHandle, useState } from "react";
+import Flexmonster from "flexmonster";
+import authHeader from '../services/auth-header';
 
-import UserService from "../services/user.service";
-import EventBus from "../common/EventBus";
-import FlexMons from "./FlexMons";
+export function BoardAdmin(props, ref) {
+    const flexmonsterRef = useRef()
+    const [user, setUser] = useState()
+    const [checkAdmin, setCheckAdmin] = useState(true)
+    const [message, setMessage] = useState('')
+
+    let flexmonster = null;
+    useEffect(() => {
+
+      let user = localStorage.getItem('user')
+      setUser(user)
+
+        const getData = async () =>{
+
+            const api ="http://localhost:8080/api/test/admin";
+
+            let data = await fetch(api,  { headers: authHeader() });
+            let res = await data.json();
+            // console.log(data);
+
+            if(res.message == "Require Admin Role!"){
+                setCheckAdmin(false)
+                setMessage('Require Admin Role!')
+            }
 
 
-const BoardAdmin = () => {
-  const [content, setContent] = useState("");
+          if(res.recordset){
+        flexmonster = new Flexmonster({
+            ...props,
+            container: flexmonsterRef.current,
+            componentFolder: "https://cdn.flexmonster.com/",
+            toolbar:true,
+            width: "100%",
+            report: {
+                dataSource: {
+                        /* URL to the Data Compressor Node.js */
+                       // filename: "http://localhost:5000/",
+                       data:res.recordset
+                   },
+                   
+                   slice: {
+
+                   columns:[{
+                       uniqueName: "MonthNam"
+
+                   }],
+                  
+                   rows: [{
+                       uniqueName: "Team"
+                   },
+                   
+                   {
+                    uniqueName: "ProdDesc"
+
+                },
+                {
+                  uniqueName: "TerritoryCode"
+
+              }
+                   ],
+                   measures: [{
+                    "uniqueName": "TargetUnit",
+                       "aggregation": "sum"
+                   },{
+                       "uniqueName": "SalesUnit",
+                       "aggregation": "sum"
+                   }
+                   
+                   
+                  ]
+                   },      
+                }
+
+        })}
 
 
-  useEffect(() => {
 
-    
-
-    UserService.getAdminBoard().then(
-      (response) => {
-        setContent(response.data);
-      },
-      (error) => {
-        const _content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-
-        setContent(_content);
-
-        if (error.response && error.response.status === 401) {
-          EventBus.dispatch("logout");
+    }
+    getData()
+        if(flexmonster){
+        return () =>  flexmonster.dispose() 
         }
-      }
-    );
-  }, []);
+    }, [])
 
- 
-  return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>{content}</h3>
-        
-      </header>
-    </div>
-  );
-};
+    useImperativeHandle(ref, () => ({
+        flexmonster: () => {
+            return flexmonster;
+        }
+    }));
 
-export default BoardAdmin;
+    BoardAdmin.propTypes = {
+        afterchartdraw: PropTypes.func,
+        aftergriddraw: PropTypes.func,
+        beforegriddraw: PropTypes.func,
+        beforetoolbarcreated: PropTypes.func,
+        cellclick: PropTypes.func,
+        celldoubleclick: PropTypes.func,
+        chartclick: PropTypes.func,
+        componentFolder: PropTypes.string,
+        customizeAPIRequest: PropTypes.func,
+        customizeCell: PropTypes.func,
+        customizeChartElement: PropTypes.func,
+        customizeContextMenu: PropTypes.func,
+        datachanged: PropTypes.func,
+        dataerror: PropTypes.func,
+        datafilecancelled: PropTypes.func,
+        dataloaded: PropTypes.func,
+        drillthroughclose: PropTypes.func,
+        drillthroughopen: PropTypes.func,
+        fieldslistclose: PropTypes.func,
+        fieldslistopen: PropTypes.func,
+        filterclose: PropTypes.func,
+        filteropen: PropTypes.func,
+        fullscreen: PropTypes.func,
+        global: PropTypes.object,
+        height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        licenseKey: PropTypes.string,
+        loadingdata: PropTypes.func,
+        loadinglocalization: PropTypes.func,
+        loadingolapstructure: PropTypes.func,
+        loadingreportfile: PropTypes.func,
+        localizationerror: PropTypes.func,
+        localizationloaded: PropTypes.func,
+        olapstructureerror: PropTypes.func,
+        olapstructureloaded: PropTypes.func,
+        openingreportfile: PropTypes.func,
+        querycomplete: PropTypes.func,
+        queryerror: PropTypes.func,
+        ready: PropTypes.func,
+        report: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+        reportchange: PropTypes.func,
+        reportcomplete: PropTypes.func,
+        reportfilecancelled: PropTypes.func,
+        reportfileerror: PropTypes.func,
+        reportfileloaded: PropTypes.func,
+        runningquery: PropTypes.func,
+        sortFieldsList: PropTypes.func,
+        toolbar: PropTypes.bool,
+        unauthorizederror: PropTypes.func,
+        update: PropTypes.func,
+        width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    }
+
+    // console.log(checkAdmin);
+    return <div>
+      
+      { (user != null) && checkAdmin == true ?
+       <div ref={flexmonsterRef}>Loading...</div>
+        : <div>
+            { !checkAdmin ? 
+        <div><h3>{message}</h3></div> 
+        :<div><h3>Login to access!</h3></div> }
+        </div>  }
+    </div> 
+}
+
+export default BoardAdmin = React.forwardRef(BoardAdmin);
